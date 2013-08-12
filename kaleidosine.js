07f79c65b93bb828669769dc59a737bcad0f735a
Andrew Langdon <andrewlngdn@gmail.com>
2013-08-12 14:36:35 -0700

    decided to use canvas. included rAnimationFrame shiv

--- kaleidosine.js
+++ kaleidosine.js
@@ -2,6 +2,10 @@ var Kaleidosine = {
 	context: undefined,
 	init: function(){
 		Kaleidosine.context = new webkitAudioContext();
+		var canvas = document.getElementById('canvas');
+		var canvasContext = canvas.getContext('2d');
+		canvasContext.fillStyle = 'red';
+		canvasContext.fillRect(200, 100, 400, 400);
 	}
 }
 
